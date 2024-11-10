50aedae6c351469022ad117d7f0ac77ea3b50dac
PLNumber <sdf1034@gmail.com>
2024-11-10 21:32:23 +0900

    바꾼거임 ㅇㅇ 창 여러개 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(const MyApp());
 }
 
+/*앱*/
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 
-
+/*메인 화면*/
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
@@ -33,7 +34,9 @@ class MainPage extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              /*이미지 창*/
               Container(
+                child: Text("이미지 siuuuu"),
                 alignment: Alignment.center,
                 padding: EdgeInsets.all(50),
                 color: Colors.amber[600],
@@ -53,8 +56,12 @@ class MainPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20.0)
                           )
                       ),
-                      onPressed: (){},
-                      child: const Center(child: Text("플레이어와 대결")))
+                      onPressed: (){
+                        Navigator.push(
+                            context, MaterialPageRoute(builder: (context) => BattlePage())
+                        );
+                      },
+                      child: const Center(child: Text("대전")))
               ),
               SizedBox(height: 20),
 
@@ -68,7 +75,11 @@ class MainPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20.0)
                           )
                       ),
-                      onPressed: (){},
+                      onPressed: (){
+                        Navigator.push(
+                            context, MaterialPageRoute(builder: (context) => QuizPage())
+                        );
+                      },
                       child: const Center(child: Text("문해력 문제")))
               ),
               SizedBox(height: 20),
@@ -83,7 +94,11 @@ class MainPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20.0)
                           )
                       ),
-                      onPressed: (){},
+                      onPressed: (){
+                        Navigator.push(
+                            context, MaterialPageRoute(builder: (context) => DictPage())
+                        );
+                      },
                       child: const Center(child: Text("단어 사전")))
               ),
               SizedBox(height: 20),
@@ -98,7 +113,9 @@ class MainPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20.0)
                           )
                       ),
-                      onPressed: (){},
+                      onPressed: (){
+                       
+                      },
                       child: const Center(child: Text("프로필 수정")))
               ),
               SizedBox(height: 20),
@@ -109,16 +126,21 @@ class MainPage extends StatelessWidget {
                 child: IconButton(
                   icon: const Icon(Icons.settings_outlined),
                   selectedIcon: const Icon(Icons.settings),
-                  onPressed: () { },
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => OptionPage())
+                    );
+                  },
                 ),
               ),
 
             /*광고 배너*/
             Container(
+              child: Text("광고 배너"),
               alignment: Alignment.center,
-              padding: EdgeInsets.all(50),
               color: Colors.red[100],
               height: 50,
+
             ),
             ],
           ),
@@ -127,3 +149,136 @@ class MainPage extends StatelessWidget {
       );
   }
 }
+
+/*대전 페이지*/
+class BattlePage extends StatelessWidget {
+  const BattlePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("대전 창"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Text('대전 광역시'),
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(50),
+            color: Colors.orange[600],
+            width: 400,
+            height: 400,
+          ),
+        )
+    );
+
+  }
+}
+
+
+/*퀴즈 페이지*/
+class QuizPage extends StatelessWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("퀴즈 창"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Text('퀴즈 어쩌구'),
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(50),
+            color: Colors.orange[600],
+            width: 400,
+            height: 400,
+          ),
+        )
+    );
+
+  }
+}
+
+/*사전 페이지*/
+class DictPage extends StatelessWidget {
+  const DictPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("사전 창"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Text('사전 어쩌구'),
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(50),
+            color: Colors.orange[600],
+            width: 400,
+            height: 400,
+          ),
+        )
+    );
+
+  }
+}
+
+/*프로필 페이지*/
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("프로필 창"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Text('프로필 어쩌구'),
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(50),
+            color: Colors.orange[600],
+            width: 400,
+            height: 400,
+          ),
+        )
+    );
+
+  }
+}
+
+
+/*옵션 페이지*/
+class OptionPage extends StatelessWidget {
+  const OptionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("옵션 창"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Text('설정 어쩌구'),
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(50),
+          color: Colors.orange[600],
+          width: 400,
+          height: 400,
+        ),
+      )
+    );
+
+  }
+}
+
