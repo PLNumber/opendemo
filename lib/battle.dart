da06cc758ff6faf15683a0bc633f626958171ecc
PLNumber <sdf1034@gmail.com>
2024-11-10 23:19:26 +0900

    바꾼거임 ㅇㅇ 창 여러개 추가 폴더 추가 프로필 변?경 a?F

--- lib/battle.dart
+++ lib/battle.dart
@@ -5,16 +5,98 @@ import 'package:flutter/material.dart';
 class BattlePage extends StatelessWidget {
   const BattlePage({Key? key}) : super(key: key);
 
+  final score = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("대전 페이지"),
+        centerTitle: true,
+      ),
+
+      body: Center(
+          child : Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+
+              Container(
+                width: 400.0,
+                height: 400.0,
+                color: Colors.pinkAccent,
+                child: Text('프로필 사진'),
+              ),
+              SizedBox(height: 30.0),
+
+              Text("$score 점",
+                style: TextStyle(fontSize: 30.0),),
+              SizedBox(height: 30.0),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  
+                  /*pvp 버튼*/
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(175,175),
+                      backgroundColor: Colors.greenAccent,
+                      padding: EdgeInsets.all(20),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)
+                      )
+                    ),
+
+                    onPressed: (){
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => PVPPage())
+                      );
+                    },
+
+                    child: Text("PVP")),
+                  SizedBox(width: 30.0),
+
+                  /*pve버튼*/
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(175,175),
+                        backgroundColor: Colors.greenAccent,
+                        padding: EdgeInsets.all(20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)
+                        )
+                    ),
+                    onPressed: (){
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => PVEPage())
+                      );
+                    },
+                    child: Text("PVE")),
+                ],
+              )
+            ],
+          )
+      ),
+    );
+  }
+}
+
+
+/*pvp 페이지*/
+class PVPPage extends StatelessWidget {
+  const PVPPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("대전 창"),
+          title: Text("pvp 창"),
           centerTitle: true,
         ),
         body: Center(
           child: Container(
-            child: Text('대전 광역시'),
+            child: Text('어쩌구'),
             alignment: Alignment.center,
             padding: EdgeInsets.all(50),
             color: Colors.orange[600],
@@ -26,3 +108,29 @@ class BattlePage extends StatelessWidget {
 
   }
 }
+
+/*pve 페이지*/
+class PVEPage extends StatelessWidget {
+  const PVEPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("pve 창"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Text('어쩌구'),
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(50),
+            color: Colors.orange[600],
+            width: 400,
+            height: 400,
+          ),
+        )
+    );
+  }
+}
+
