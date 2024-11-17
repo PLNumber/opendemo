ff10115e099182842c0fe903947e480ce4e26fe4
PLNumber <sdf1034@gmail.com>
2024-11-17 20:03:49 +0900

    퀴즈 없는동안 부탁좀

--- lib/Function/class.dart
+++ lib/Function/class.dart
@@ -1,10 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Question{
-  num w_Id;
-
+class Question {
+  int wordId;
+  bool isCorrect;
   String word;
   String definition;
-  bool iscorrect;
-  Question(this.w_Id, this.word, this.definition, {this.iscorrect = true});
+
+  Question(this.wordId, this.word, this.definition, {this.isCorrect = true});
+
+  // 객체를 Map으로 변환
+  Map<String, dynamic> toMap() {
+    return {
+      'wordId': wordId,
+      'isCorrect': isCorrect,
+      'word': word,
+      'definition': definition,
+    };
+  }
+
+  // Firestore에서 가져온 Map을 객체로 변환
+  factory Question.fromMap(Map<String, dynamic> map) {
+    return Question(
+      map['wordId'] as int,
+      map['word'] as String,
+      map['definition'] as String,
+      isCorrect: map['isCorrect'] as bool? ?? true,
+    );
+  }
 }
+
