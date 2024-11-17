d61c875187fc2a29592648328a0f6738339894f1
PLNumber <sdf1034@gmail.com>
2024-11-17 19:57:02 +0900

    퀴즈 없는동안 부탁좀

--- lib/Function/class.dart
+++ lib/Function/class.dart
@@ -1,31 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Question {
-  int wordId;
-  bool isCorrect;
+class Question{
+  num w_Id;
+
   String word;
   String definition;
-
-  Question(this.wordId, this.word, this.definition, {this.isCorrect = true});
-
-  // 객체를 Map으로 변환
-  Map<String, dynamic> toMap() {
-    return {
-      'wordId': wordId,
-      'isCorrect': isCorrect,
-      'word': word,
-      'definition': definition,
-    };
-  }
-
-  // Firestore에서 가져온 Map을 객체로 변환
-  factory Question.fromMap(Map<String, dynamic> map) {
-    return Question(
-      map['wordId'] as int,
-      map['word'] as String,
-      map['definition'] as String,
-      isCorrect: map['isCorrect'] as bool? ?? true,
-    );
-  }
+  bool iscorrect;
+  Question(this.w_Id, this.word, this.definition, {this.iscorrect = true});
 }
-
--- lib/Pages/quiz.dart
+++ lib/Pages/quiz.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:opendemo/Function/class.dart';
 import 'package:xml/xml.dart';
 import '../Function/create_collection.dart';
-import '../Function/class.dart';
 
 /*퀴즈 메인 페이지*/
 class QuizMainPage extends StatelessWidget {
@@ -71,173 +69,6 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Question> _questions = [];
-  int _currentQuestionIndex = 0;
-  final TextEditingController _answerController = TextEditingController();
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchQuestions(); // Firestore 데이터 가져오기
-  }
-
-  Future<void> _fetchQuestions() async {
-    try {
-      final snapshot = await FirebaseFirestore.instance.collection('questions').get();
-      final questions = snapshot.docs
-          .map<Question>((doc) => Question.fromMap(doc.data() as Map<String, dynamic>))
-          .toList();
-      setState(() {
-        _questions = questions;
-        _isLoading = false;
-      });
-    } catch (e) {
-      print('Error fetching questions: $e');
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isLoading) {
-      return Scaffold(
-        appBar: AppBar(title: Text('퀴즈 풀기')),
-        body: Center(child: CircularProgressIndicator()),
-      );
-    }
-
-    if (_questions.isEmpty) {
-      return Scaffold(
-        appBar: AppBar(title: Text('퀴즈 풀기')),
-        body: Center(child: Text('퀴즈 데이터가 없습니다.')),
-      );
-    }
-
-    final currentQuestion = _questions[_currentQuestionIndex];
-
-    return Scaffold(
-      appBar: AppBar(title: Text('퀴즈 풀기')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "문제: ${currentQuestion.definition}",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _answerController,
-              decoration: InputDecoration(
-                labelText: '정답을 입력하세요',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _checkAnswer(currentQuestion),
-              child: Text('제출'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _checkAnswer(Question question) {
-    String userAnswer = _answerController.text.trim();
-
-    setState(() {
-      if (userAnswer.toLowerCase() == question.word.toLowerCase()) {
-        _showResultDialog(true);
-      } else {
-        question.isCorrect = false;
-        _saveWrongAnswer(question); // 오답 저장
-        _showResultDialog(false);
-      }
-      _answerController.clear();
-    });
-  }
-
-  void _saveWrongAnswer(Question question) async {
-    final docRef = FirebaseFirestore.instance.collection('wrongAnswers').doc();
-    await docRef.set(question.toMap());
-  }
-
-
-  void _showResultDialog(bool isCorrect) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(isCorrect ? '정답입니다!' : '오답입니다.'),
-          content: Text(
-            isCorrect
-                ? '잘했습니다! 다음 문제로 넘어갑니다.'
-                : '정답은 "${_questions[_currentQuestionIndex].word}"입니다.',
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-                _moveToNextQuestion();
-              },
-              child: Text('다음'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _moveToNextQuestion() {
-    setState(() {
-      if (_currentQuestionIndex < _questions.length - 1) {
-        _currentQuestionIndex++;
-      } else {
-        _showCompletionDialog();
-      }
-    });
-  }
-
-  void _showCompletionDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('퀴즈 완료'),
-          content: Text('모든 문제를 푸셨습니다!'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.pop(context);
-              },
-              child: Text('메인으로'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-
-
-
-class NotePage extends StatelessWidget {
-  const NotePage({Key? key}) : super(key: key);
-
-  Future<List<Question>> _fetchWrongAnswers() async {
-    final snapshot = await FirebaseFirestore.instance.collection('wrongAnswers').get();
-    return snapshot.docs.map((doc) => Question.fromMap(doc.data())).toList();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -246,30 +77,67 @@ class NotePage extends StatelessWidget {
         title: Text('오답노트'),
         centerTitle: true,
       ),
-      body: FutureBuilder<List<Question>>(
-        future: _fetchWrongAnswers(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('오류 발생: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('저장된 오답이 없습니다.'));
-          }
-
-          final wrongAnswers = snapshot.data!;
-          return ListView.builder(
-            itemCount: wrongAnswers.length,
-            itemBuilder: (context, index) {
-              final question = wrongAnswers[index];
-              return ListTile(
-                title: Text('문제: ${question.definition}'),
-                subtitle: Text('정답: ${question.word}'),
-              );
-            },
-          );
-        },
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("예시1"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("예시2"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("예시3"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("예시4"),
+            onTap: (){},
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
+
+
+/*오답노트 페이지*/
+class NotePage extends StatelessWidget {
+  const NotePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('오답노트'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("예시1"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("예시2"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("예시3"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("예시4"),
+            onTap: (){},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
